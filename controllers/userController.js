2fcd0cf30c6d28d301b90d16913eff8b1a959650
Jessye Lam <jessyel13@gmail.com>
2024-06-06 15:25:16 -0400

    changed user api to properly check and use query param

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,18 @@
 const User = require("../models/user");
 const ObjectId = require('mongoose').Types.ObjectId;
 
-const { body, validationResult } = require("express-validator");
+const { query, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 exports.search = [
-  body("user", "content must be specified").trim().isLength({ min: 1 }).escape(),
+  query("user", "content must be specified").trim().isLength({ min: 1 }).escape(),
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.sendStatus(500)
     }
     else {
-      const allUsers = await User.find({ $text: { $search: req.params.user } }, { _id: 1, username: 1 }).sort({ username: 1 }).exec();
+      const allUsers = await User.find({ $text: { $search: req.query.user } }, { _id: 1, username: 1 }).sort({ username: 1 }).exec();
       res.json({ users: allUsers })
     }
   }),];
